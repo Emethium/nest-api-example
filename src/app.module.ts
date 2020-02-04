e548ea46c97c28bb56a521a25aadcf43ab629846
emethium <nikolas.serafini@gmail.com>
2020-02-04 15:30:52 -0300

    feat(entities): updated GenericEntity module and app.module files

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,19 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EntitiesModule } from './entities/entities.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [EntitiesModule],
+  imports: [
+    EntitiesModule,
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'db',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
--- src/entities/entities.module.ts
+++ src/entities/entities.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { EntitiesService } from './entities/entities.service';
 import { EntitiesController } from './entities/entities.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { GenericEntity } from './entities/entity.entity'
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([GenericEntity]),
+  ],
   providers: [EntitiesService],
   controllers: [EntitiesController]
 })
