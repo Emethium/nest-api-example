f27141c0b2344794b9d010631a8046df9cd6a05f
emethium <nikolas.serafini@gmail.com>
2020-02-04 15:30:06 -0300

    feat(entities.controller): created controller for GenericEntity CRUD operations

--- src/entities/entities/entities.controller.ts
+++ src/entities/entities/entities.controller.ts
@@ -1,9 +1,29 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete, Body, Param } from '@nestjs/common';
+import { EntitiesService } from './entities.service';
+import { GenericEntity } from './entity.entity';
 
 @Controller('entities')
 export class EntitiesController {
+  constructor(private entitiesService: EntitiesService) {}
+
   @Get()
-  index(): string {
-    return "Fucking monads"
+  index(): Promise<GenericEntity[]> {
+    return this.entitiesService.findAll();
   }
+
+  @Post('create')
+  async create(@Body() entitytData: GenericEntity): Promise<any> {
+    return this.entitiesService.create(entitytData);
+  }
+
+  @Put(':id/update')
+  async update(@Param('id') id, @Body() entityData: GenericEntity): Promise<any> {
+      entityData.id = Number(id);
+      return this.entitiesService.update(entityData);
+  }
+
+  @Delete(':id/delete')
+    async delete(@Param('id') id): Promise<any> {
+      return this.entitiesService.delete(id);
+  }  
 }
