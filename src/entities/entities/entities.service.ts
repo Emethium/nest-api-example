12cc9ae74002d1d29b2150b6d5bde0c8d8b0787a
emethium <nikolas.serafini@gmail.com>
2020-02-04 15:28:57 -0300

    feat(entities.service): created service for dealing with GenericEntity repository operation

--- src/entities/entities/entities.service.ts
+++ src/entities/entities/entities.service.ts
@@ -1,4 +1,29 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm'
+import { Repository } from 'typeorm';
+import { UpdateResult, DeleteResult } from 'typeorm';;
+import { GenericEntity } from './entity.entity'
 
 @Injectable()
-export class EntitiesService {}
+export class EntitiesService {
+  constructor(
+    @InjectRepository(GenericEntity)
+    private entityRepository: Repository<GenericEntity>
+  ) { }
+
+  async findAll(): Promise<GenericEntity[]> {
+    return await this.entityRepository.find();
+  }
+
+  async create(entity: GenericEntity): Promise<GenericEntity> {
+    return await this.entityRepository.save(entity);
+  }
+
+  async update(entity: GenericEntity): Promise<UpdateResult> {
+    return await this.entityRepository.update(entity.id, entity)
+  }
+
+  async delete(id): Promise<DeleteResult> {
+    return await this.entityRepository.delete(id);
+  }
+}
